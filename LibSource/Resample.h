2a79566847eebb671e6172fc0c4d5f223015cd89
Damien Quartz <dcomposer@gmail.com>
2022-03-03 20:12:06 -0800

    use setLowShelf to configure resampler filters

--- LibSource/Resample.h
+++ LibSource/Resample.h
@@ -82,10 +82,11 @@ public:
     BiquadFilter* filter = BiquadFilter::create(sr, stages);
     // [B, A]=cheby1(2, 2, 0.25); then use [B, -A(2:end)] , note the minus sign in front of the A coefficients!!!!
     //alternatively, we could use FilterStage to compute the coefficients
-    float upCoeffs[5]= {0.07609109, 0.15218218, 0.07609109, +1.16511283,  -0.54828486};
-    for(int n=0; n<3; n++)
-      upCoeffs[n] *= factor; //compensate for the gain loss due to zero-stuffing, gives unitary gain after upsampling
-    filter->copyCoefficients(FloatArray(upCoeffs, 5));
+    //float upCoeffs[5]= {0.07609109, 0.15218218, 0.07609109, +1.16511283,  -0.54828486};
+    //for(int n=0; n<3; n++)
+    //  upCoeffs[n] *= factor; //compensate for the gain loss due to zero-stuffing, gives unitary gain after upsampling
+    //filter->copyCoefficients(FloatArray(upCoeffs, 5));
+    filter->setLowShelf(sr / factor * 0.5f, 0.0f);
     return new UpSampler(filter, factor);
   }
   static void destroy(UpSampler* obj){
@@ -120,8 +121,9 @@ public:
     BiquadFilter* filter = BiquadFilter::create(sr, stages);
     // [B, A]=cheby1(2, 2, 0.25); then use [B, -A(2:end)] , note the minus sign in front of the A coefficients!!!!
     //alternatively, we could use FilterStage to compute the coefficients
-    static float downCoeffs[5]={0.07609109, 0.15218218, 0.07609109, +1.16511283,  -0.54828486};
-    filter->copyCoefficients(FloatArray(downCoeffs,5));
+    //static float downCoeffs[5]={0.07609109, 0.15218218, 0.07609109, +1.16511283,  -0.54828486};
+    //filter->copyCoefficients(FloatArray(downCoeffs,5));
+    filter->setLowShelf(sr / factor * 0.5f, 0.0f);
     return new DownSampler(filter, factor);
   }
   static void destroy(DownSampler* obj){
